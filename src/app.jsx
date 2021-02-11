c6e13e77236f6db1f67a41a79e75f78211088730
JMK1007 <coxld010@naver.com>
2021-02-12 02:55:55 +0900

    PureComponent

--- src/app.jsx
+++ src/app.jsx
@@ -13,19 +13,23 @@ class App extends Component {
   };
 
   handleIncrement = (habit) => {
-    console.log(`Increment ${habit.name} ${habit.count}`);
-    const habits = [...this.state.habits];
-    const index = habits.indexOf(habit);
-    habits[index].count++;
+    const habits = this.state.habits.map((item) => {
+      if (item.id === habit.id) {
+        return { ...habit, count: habit.count + 1 };
+      }
+      return item;
+    });
     this.setState({ habits });
   };
 
   handleDecrement = (habit) => {
-    console.log(`Decrement ${habit.name} ${habit.count}`);
-    const habits = [...this.state.habits];
-    const index = habits.indexOf(habit);
-    const count = habits[index].count - 1;
-    habits[index].count = count < 0 ? 0 : count;
+    const habits = this.state.habits.map((item) => {
+      if (item.id === habit.id) {
+        const count = habit.count - 1;
+        return { ...habit, count: count < 0 ? 0 : count };
+      }
+      return item;
+    });
     this.setState({ habits });
   };
 
@@ -40,15 +44,11 @@ class App extends Component {
     this.setState({ habits });
   };
 
-  handleNav = () => {
-    const totalCount = this.state.habits.filter((item) => item.count > 0)
-      .length;
-    this.setState({ totalCount });
-  };
-
   handleReset = () => {
     const habits = this.state.habits.map((habit) => {
-      habit.count = 0;
+      if (habit.count !== 0) {
+        return { ...habit, count: 0 };
+      }
       return habit;
     });
     this.setState({ habits });
